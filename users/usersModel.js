e7f19d3c133635d2d70ed4307f92dfb585ea5f73
Eli Citron <ebcitron@gmail.com>
2019-05-23 10:26:11 -0500

    Sanity Commit

--- users/usersModel.js
+++ users/usersModel.js
@@ -1,1 +1,35 @@
-const db = require('../database/dbConfig');
+// Sets up functions to call specific sqlite3 Database querrys
+
+// Import the database Configuration
+
+const db = require("../database/dbConfig");
+
+// Create Querry Functions
+
+function find() {
+  return db("users").select("id", "username", "password", "department");
+}
+
+function findBy(filter) {
+  return db("users").where(filter);
+}
+
+async function add(user) {
+  const [id] = await db("users").insert(user);
+  return findById(id);
+}
+
+function findById(id) {
+  return db("users")
+    .where({ id })
+    .first();
+}
+
+// Export Functions
+
+module.exports = {
+  add,
+  find,
+  findBy,
+  findById
+};
